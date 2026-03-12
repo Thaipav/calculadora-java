6258759765f8db4458a97ecdd5a6882377a12076
Thaína Pavone <pavonethaina@gmail.com>
2026-03-12 17:28:06 -0300

    Projeto calculadora em Java com interface

---
+++ Trabalho1_prj_calculadora/src/trabalho1_prj_calculadora/Operacoes.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalho1_prj_calculadora;
+
+/**
+ *
+ * @author Thainá Pavone
+ */
+public class Operacoes {
+
+    public double soma(double a, double b) {
+        return a + b;
+    }
+
+    public double subtracao(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicacao(double a, double b) {
+        return a * b;
+    }
+
+    public double divisao(double a, double b) {
+        return a / b;
+    }
+}
